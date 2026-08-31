4113567e03a4478d8b3d00623a3774d3b8cef0aa
xujingxuan69-web <xujingxuan69@gmail.com>
2026-08-31 08:43:01 +0800

    Temp

---
+++ Assets/Scripts/Input/PlayerInputReader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    public Vector2 MoveInput { get; private set; }
+    public float MoveSpeed { get; private set; }
+
+
+}
