367fb46423eebc7d0e081d0bfa97541bbd1f0752
Emo <xujingxuan69@gmail.com>
2026-09-02 17:54:54 +0800

    temp

--- Assets/Scripts/Input/PlayerInputReader.cs
+++ Assets/Scripts/Input/PlayerInputReader.cs
@@ -1,16 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputReader : MonoBehaviour
 {
-    private PlayerInputActions inputActions;
+    private PlayerInputActions inputActions = new PlayerInputActions();
 
     public Vector2 MoveInput { get; private set; }
     public bool JumpPressed { get; private set; }
 
-    private void Awake()
+    private void OnEnable()
     {
-        inputActions = new PlayerInputActions();
+        inputActions.Player_Horse.Enable();
+        inputActions.Player_Horse.Jump.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player_Horse.Jump.performed -= OnJump;
+        inputActions.Player_Horse.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveInput = inputActions.Player_Horse.Move.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        JumpPressed = true;
+    }
+
+    public bool ConsumeJumpPressed()
+    {
+        if (!JumpPressed) return false;
+        JumpPressed = false;
+        return true;
+    }
+
+    private void OnDestroy()
+    {
+        inputActions?.Dispose();
     }
 }
